1c750dc018d09cda4d821181da8f3e3fa00645ee
liammx <liammx03@gmial.com>
2023-10-28 14:58:28 +0500

    js12

---
+++ main.js
@@ -0,0 +1,121 @@
+// let txt = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel architecto quibusdam excepturi, consequuntur tempore nulla tempora laboriosam nemo ratione asperiores nesciunt rerum sed ipsum ullam amet sequi veniam quia mollitia inventore distinctio harum numquam voluptatibus maxime. Voluptate porro quae sunt accusantium saepe hic nobis libero sint neque quidem? Tempora deleniti ea deserunt cupiditate iste dicta quam nisi. Reprehenderit tenetur cupiditate cumque fuga recusandae corporis omnis velit, tempora laboriosam. Deleniti magnam debitis quos unde nostrum amet veritatis laborum beatae voluptates dolores sapiente blanditiis, facilis, iure voluptatem possimus pariatur commodi consequatur quo earum voluptas odit quod. Tenetur eos cum quasi rem nemo!"
+
+// let t = txt.toLowerCase().split('')
+
+// let s = []
+// let a = []
+// let l = []
+// let o = []
+// let m = []
+
+// for(let i = 0; i < t.length; i++) {
+//     if(t[i] === s) {
+//         s.push(t[i])
+//     }
+//     if (t[i] === a) {
+//         a.push(t[i])
+//     }
+//     if(t[i] === l) {
+//         l.push(t[i])
+//     }
+//     if(t[i] === o) {
+//         o.push(t[i])
+//     }
+//     if(t[i] === m) {
+//         m.push(t[i])
+//     }
+// }
+ 
+// console.log(s);
+// console.log(a);
+// console.log(l);
+// console.log(o);
+// console.log(m);
+// let b = Math.min(s.length, a.length, l.length, o.length, m.length)
+// console.log(b);
+
+// let o = []
+
+// for(let i = 0; i < t.length; i++) {
+    // if(t[i] === o) {
+    //     o.push(t[i])
+    // }
+// }
+
+// console.log(o.length);
+
+
+let users = [
+    {
+        name: "Alex",
+        age: 18
+    },
+    {
+        name: "Alex",
+        age: 20
+    },
+    {
+        name: "Alex",
+        age: 80
+    },
+    {
+        name: "Alex",
+        age: 85
+    },
+    {
+        name: "Alex",
+        age: 78
+    },
+    {
+        name: "Alex",
+        age: 15
+    },
+    {
+        name: "Alex",
+        age: 17
+    },
+    {
+        name: "Alex",
+        age: 16
+    },
+    {
+        name: "Alex",
+        age: 50
+    },
+    {
+        name: "Alex",
+        age: 13
+    },
+    {
+        name: "Alex",
+        age: 17
+    },
+    {
+        name: "Alex",
+        age: 55
+    },
+    {
+        name: "Alex",
+        age: 67
+    },
+    {
+        name: "Alex",
+        age: 79
+    },
+]
+
+let katta = []
+let kichik = []
+// let t = users.slice('')
+
+for(let i = 0; i < users.length; i++) {
+    if(users[0].age[i] >= katta) {
+        katta.push(users[i])
+    }
+}
+
+console.log(katta);
+console.log(users[0].age);
+// console.log(t);
+// console.log(users);
+// console.log(i);
